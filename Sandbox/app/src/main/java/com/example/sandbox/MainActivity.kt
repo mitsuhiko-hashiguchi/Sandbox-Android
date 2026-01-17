3f588eb84c67a4bbaef5d7cd3eb779fa6b7558f7
mitsuhiko-hashiguchi <kgrzkyk0827@gmail.com>
2026-01-17 21:09:44 +0900

    Feature/Navigation

--- Sandbox/app/src/main/java/com/example/sandbox/MainActivity.kt
+++ Sandbox/app/src/main/java/com/example/sandbox/MainActivity.kt
@@ -8,6 +8,9 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.example.sandbox.ui.theme.SandboxTheme
 
 class MainActivity : ComponentActivity() {
@@ -16,14 +19,51 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             SandboxTheme {
-                HomeScreen()
+                AppNavigation()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+fun AppNavigation() {
+    val navController = rememberNavController()
+
+    NavHost(
+        navController = navController,
+        startDestination = "home" // 最初に表示する画面の名前
+    ) {
+        // ホーム画面
+        composable(
+            route = "home"
+        ) {
+            HomeScreen(
+                onNavigateToList = {
+                    // ボタンが押されたら "list" へ遷移
+                    navController.navigate(route = "list")
+                }
+            )
+        }
+
+        // リスト画面
+        composable(
+            route = "list"
+        ) {
+            ListScreen(
+                onBack = {
+                    // 戻る操作
+                    navController.popBackStack()
+                }
+            )
+        }
+    }
+}
+
+@Composable
+fun Greeting(
+    name: String,
+    modifier: Modifier = Modifier
+) {
     Text(
         text = "Hello $name!",
         modifier = modifier
@@ -34,6 +74,8 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     SandboxTheme {
-        Greeting("Android")
+        Greeting(
+            name = "Android"
+        )
     }
 }
